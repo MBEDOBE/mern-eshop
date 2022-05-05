91a7fb849e84f8283a33bf5810e8250dfd0c15b9
MBEDOBE <danielbushbaby@gmail.com>
2022-05-05 16:06:19 +0000

    data-from-backend

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 # What am i learning?
 3. create static data and displaying on home screen
 4. routing with React-Router-Dom
-5. backend server with express and nodemon for fast reload,
+5. backend server with express and nodemon for fast reload
+6. fetch products from backend (ajax request) using axios
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//import data from '../data';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
