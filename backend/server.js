8c0aa3663b227ea4125442d85a975f5ab5e91f8a
MBEDOBE <danielbushbaby@gmail.com>
2022-06-14 22:17:01 +0000

    userProfile functionality

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/keys/paystack', (req, res) => {
+app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
