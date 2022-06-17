0119bdd5d14656385b25600ed2a65767b7dd6660
MBEDOBE <danielbushbaby@gmail.com>
2022-06-17 04:55:31 +0000

    heroku-setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -34,6 +34,12 @@ app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
+});
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, '/frontend/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
